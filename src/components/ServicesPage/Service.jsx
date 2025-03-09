43194d172eb93e98cbcdfa8b719c7dfb4cea91af
arunS11 <iarunskumar11@gmail.com>
2025-03-09 23:05:59 +0530

    modified service page responsive layout

--- src/components/ServicesPage/Service.jsx
+++ src/components/ServicesPage/Service.jsx
@@ -1,6 +1,6 @@
 import React,{useState, useRef} from 'react'
 import './Service.css'
-import Anim from '../../assets/byt.gif'
+import Anim from '../../assets/service-anim.gif'
 import BackgroundImage from '../../assets/name.png'
 
 const services = [
@@ -53,21 +53,14 @@ function Service() {
     <div className='servicePage'>
         <div className='left-side'>
           <div className='left-text'>
-          <h1>EXPLORE OUR</h1>
-          <h2>SERVICES</h2>
+            <h1>EXPLORE OUR</h1>
+            <h2>SERVICES</h2>
           </div>
-          {/* <p>Welcome to BYTEHIVE, where cutting-edge technology meets innovative solutions. We specialize in a wide range of IT services, including custom software development, cloud computing, cybersecurity, and IT consulting. Our team of experts is dedicated to delivering reliable, scalable, and secure solutions tailored to meet the unique needs of your business. Whether you're looking to streamline your operations, protect your data, or build the next big tech solution, we’re here to help you succeed in an ever-evolving digital landscape. Let’s build the future together!</p> */}
           <p>Welcome to BYTEHIVE, where technology meets innovation. We deliver secure, scalable, and tailored IT solutions to help your business thrive in the digital era. Let’s build the future together!</p>
           <img src={Anim} alt="" className='service-img' />
         </div>
         
-        <div className='right-side'
-            style={{
-              backgroundImage: activeIndex !== null ? `url(${services[activeIndex].image})` : 'none',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-            }}
-          >
+        <div className='right-side'>
           <div className='cards-container'>
             {services.map((service, index) => (
               <div
@@ -75,7 +68,11 @@ function Service() {
                 className={`service-block ${index === activeIndex ? 'expanded' : ''}`}
                 onClick={() => handleCardClick(index)}
               >
-                <h3>{service.title}</h3>
+                <h3>
+                  {service.title.split(" ").slice(0, 2).join(" ")}
+                  {service.title.split(" ").length > 2 && <br />}
+                  {service.title.split(" ").slice(2).join(" ")}
+                </h3>
                 {index === activeIndex && <p className='service-description'>{service.description}</p>}
               </div>
             ))}
