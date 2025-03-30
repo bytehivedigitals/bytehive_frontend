97a057c394019588deb9bc499c48ca54eaaa8558
Jishnu9594 <jishnuaswin025@gmail.com>
2025-03-31 02:09:39 +0530

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CurvedNavbar from "./components/Navbar/CurvedNavbar";
 import Home from "./pages/home";
 import "./index.css";
-import ChatBot from "./components/ChatBot/ChatBot";
 
 function App() {
   return (
@@ -13,9 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-            <ChatBot />
     </Router>
-    
   );
 }
 
